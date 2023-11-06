b78a4dbefde2d3a85e98d289a72ff0708f52d896
Andrew Hancock <AndrewNeilHancock@gmail.com>
2023-11-05 20:44:24 -0500

    Added basic demonstration of SQL Alchemy introspection.

--- src/deu/db.py
+++ src/deu/db.py
@@ -3,6 +3,7 @@ import pyodbc
 from deu.repo import pass_repo, Repo
 from deu.cred import get_cred_entry, get_password
 from deu.conn import get_conn_entry
+from sqlalchemy import create_engine, MetaData, Table, inspect
 
 
 @click.group()
@@ -15,33 +16,19 @@ def db(repo: Repo) -> None:
 @click.option('--cred_key', required=True)
 @pass_repo
 def test(repo: Repo, cred_key: str) -> None:
+
     conn_entry = get_conn_entry(repo, cred_key)
     cred_entry = get_cred_entry(repo, conn_entry.cred_key)
     password = get_password(cred_entry.key, cred_entry.user_name)
 
-    conn = pyodbc.connect(_conn_str(conn_entry.host, cred_entry.user_name, password, 'AdventureWorksLT2019'))
-    sql_qry = """
-                SELECT 
-                    TOP 5 c.CustomerID, 
-                    c.CompanyName, 
-                    COUNT(soh.SalesOrderID) AS OrderCount 
-                FROM 
-                    SalesLT.Customer AS c 
-                LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID 
-                GROUP BY 
-                    c.CustomerID, 
-                    c.CompanyName 
-                ORDER BY 
-                    OrderCount DESC;
-               """
+    engine = create_engine(f"mssql+pyodbc://{cred_entry.user_name}:{password}@{conn_entry.host}/AdventureWorksLT2019?driver=SQL+Server", use_setinputsizes=False)
+    print("Inspecting database...")
+    insp = inspect(engine)
 
-    cursor = conn.cursor()
-    cursor.execute(sql_qry)
-
-    records = cursor.fetchall()
-    for r in records:
-        print(f"{r.CustomerID}\t{r.OrderCount}\t{r.CompanyName}")
-
+    for table in insp.get_table_names():
+        print(f"\nTable {table}:")
+        for c in insp.get_columns(table_name=table):
+            print(f"\t{c['name']}: {c['type']}")
 
 def _conn_str(host, username, password, database) -> str:
     return f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={host};DATABASE={database};UID={username};PWD={password}'
